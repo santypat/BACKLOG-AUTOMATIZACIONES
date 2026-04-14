874edd1bd9b7c46b4a1b5526d62e8bd10ae9778c
santypat <132416811+santypat@users.noreply.github.com>
2026-04-14 13:54:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 from supabase import create_client
-
+import os
 # -------------------------
 # SUPABASE
 # -------------------------
